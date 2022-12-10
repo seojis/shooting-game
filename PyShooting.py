1e152f43f88cc351623a76825f9f6e251fecc2a6
Seojihyun <dkapeltv@naver.com>
2022-12-10 22:58:33 +0900

    original

---
+++ PyShooting.py
@@ -0,0 +1,94 @@
+import pygame
+import sys
+from time import sleep
+
+padWidth = 480
+padHeight = 640
+
+
+def drawObject(obj, x, y):
+    global gamePad
+    gamePad.blit(obj, (x, y))
+
+
+def initGame():
+    global gamePad, clock, background, fighter, missile
+    pygame.init()
+    gamePad = pygame.display.set_mode((padWidth, padHeight))
+    pygame.display.set_caption('PyShooting')
+    background = pygame.image.load('background.png')
+    fighter = pygame.image.load('fighter.png')
+    missile = pygame.image.load('missile.png')
+    clock = pygame.time.Clock()
+
+
+def runGame():
+    global gamePad, clock, background, fighter, missile
+
+    fighterSize = fighter.get_rect().size
+    fighterWidth = fighterSize[0]
+    fighterHeight = fighterSize[1]
+
+    x = padWidth * 0.45
+    y = padHeight * 0.9
+    fighterX = 0
+
+    missileXY = []
+
+    onGame = False
+    while not onGame:
+        for event in pygame.event.get():
+            if event.type in [pygame.QUIT]:
+                pygame.quit()
+                sys.exit()
+
+            if event.type in [pygame.KEYDOWN]:
+                if event.key == pygame.K_LEFT:
+                    fighterX -= 5
+
+                elif event.key == pygame.K_RIGHT:
+                    fighterX += 5
+
+                elif event.key == pygame.K_SPACE:
+                    missileX = x + fighterWidth / 2
+                    missileY = y - fighterHeight
+                    missileXY.append([missileX, missileY])
+
+            if event.type in [pygame.KEYUP]:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    fighterX = 0
+
+        drawObject(background, 0, 0)
+
+        x += fighterX
+        if x < 0:
+            x = 0
+        elif x > padWidth - fighterWidth:
+            x = padWidth - fighterWidth
+
+        drawObject(fighter, x, y)
+
+        if len(missileXY) != 0:
+            for i, bxy in enumerate(missileXY):
+                bxy[1] -= 10
+                missileXY[i][1] = bxy[1]
+
+                if bxy[1] < - 0:
+                    try:
+                        missileXY.remove(bxy)
+                    except:
+                        pass
+
+        if len(missileXY) != 0:
+            for bx, by in missileXY:
+                drawObject(missile, bx, by)
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+    pygame.quit()
+
+
+initGame()
+runGame()
